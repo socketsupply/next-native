bd15ff977ad416cac6677742963df5e2f3384722
Joseph Werle <joseph.werle@gmail.com>
2024-04-06 03:31:30 -0400

    test(): try socket module imports

--- test/next.config.mjs
+++ test/next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 export default {
-	basePath: '/app',
-	output: 'standalone'
+  basePath: '/app',
+  output: 'standalone',
+  experimental: {
+    esmExternals: true,
+    serverComponentsExternalPackages: ['socket:fs/promises']
+  },
+
+  webpack (config, ctx) {
+    config.externals ||= []
+    config.externals.push(({ context, request }, callback) => {
+      if (/^socket:/.test(request)) {
+        return callback(null, 'module ' + request)
+      }
+      return callback()
+    })
+
+    return config
+  }
 }
--- test/src/app/hello/api/route.js
+++ test/src/app/hello/api/route.js
@@ -1,6 +1,13 @@
 import { NextResponse } from 'next/server'
+import dynamic from 'next/dynamic'
 
 export const runtime = 'edge'
+
+let os = null
+
+try {
+  os = require('node:os')
+} catch {}
 
 export async function GET (request) {
   return NextResponse.json({ hello: 'world' })
