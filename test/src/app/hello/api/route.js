9c347d565a4c40860f0a65d0e59fd80c6633ee68
Joseph Werle <joseph.werle@gmail.com>
2024-04-09 18:43:35 -0400

    refactor(test): clean up

--- test/src/app/hello/api/route.js
+++ test/src/app/hello/api/route.js
@@ -3,12 +3,6 @@ import dynamic from 'next/dynamic'
 
 export const runtime = 'edge'
 
-let os = null
-
-try {
-  os = require('node:os')
-} catch {}
-
 export async function GET (request) {
   return NextResponse.json({ hello: 'world' })
 }
