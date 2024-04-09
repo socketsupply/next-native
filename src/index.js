7da2e9620dc447f6d1aa700dc00509f6d9cc58a5
Joseph Werle <joseph.werle@gmail.com>
2024-04-09 17:03:36 -0400

    refactor(src/index.js): clean up

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import Server from './server.js'
  */
 
 /**
- * @param {CreateServerOptions} options
+ * @param {CreateServerOptions=} [options]
  * @return {Promise<Server>}
  */
-export default async function createServer (options) {
+export default async function createServer (options = null) {
   const server = new Server(options)
   await server.prepare()
   return server
